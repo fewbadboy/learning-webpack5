1bd268549b942ee15667f4928e7a9e3eb045da8d
fewbadboy <renekton.king@qq.com>
2021-04-20 01:21:13 +0800

    20200417

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-function say() {
-    console.log('say')
+import _ from 'lodash'
+import './styles/index.scss'
+
+function componnet() {
+    const element = document.createElement('div')
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('danger')
+    return element
 }
+
+document.body.appendChild(componnet())
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path')
+const TerserWebapckPlugin = require('terser-webpack-plugin')
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
     }
 }
