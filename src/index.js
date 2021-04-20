4061c47eba9f9f1afc4977be1ba1e06e3bde2b42
fewbadboy <renekton.king@qq.com>
2021-04-20 18:09:15 +0800

    20210420

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import _ from 'lodash'
 import './styles/index.scss'
 
+import printMe from './print.js';
+
+import csv from './Data/data.csv'
+import xml from './Data/data.xml'
+import yaml from './Data/data.yaml'
+import json from './Data/data.json5'
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
+console.log(csv)
+console.log(xml)
 function componnet() {
     const element = document.createElement('div')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
     element.classList.add('danger')
+    element.onclick = printMe
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path')
-const TerserWebapckPlugin = require('terser-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const yaml = require('yamljs')
+const json5 = require('json5')
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        before: './src/before.js',
+        index: './src/index.js',
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            '@src': path.resolve(__dirname, 'src')
+        }
+    },
+    devServer: {
+        contentBase: './dist'
     },
     module: {
         rules: [
@@ -21,7 +35,52 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.yaml$/i,
+                type: 'json',
+                parser: {
+                    parse: yaml.parse,
+                },
+            },
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
             }
         ]
-    }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                libs: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'chunk-libs',
+                    priority: 10,
+                    chunks: 'all',
+                }
+            }
+        },
+        runtimeChunk: 'single'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack5'
+        })
+    ]
 }
