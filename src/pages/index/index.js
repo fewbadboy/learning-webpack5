22124b541c43a45408fb730fd7f64188ed70cf8f
fewbadboy <1294234810@qq.com>
2022-07-26 01:15:38 +0800

    add

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
+import PropTypes from 'prop-types'
 import _ from 'lodash'
 
 import '../../icons'
@@ -7,16 +8,27 @@ import '../../icons'
 import '@/styles/index.scss'
 
 class Webpack extends React.Component {
+
+  static propTypes = {
+    increment: PropTypes.number
+  }
+
+  static defaultProps = {
+    increment: 1
+  }
+
   constructor(props) {
     super(props)
     this.state = { count: 0 }
     this.handleClick = this.handleClick.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
   }
 
   async handleClick(e) {
-    this.setState(state => ({
-      count: state.count + 1
+    this.setState((state, props) => ({
+      count: state.count + props.increment
     }))
+
     return await import('@/print').then(module => {
       const print = module.default
       print()
@@ -34,6 +46,15 @@ class Webpack extends React.Component {
     }
   }
 
+  handleArrowClick = (name, e) => {
+    console.log(this, e)
+    console.info(name)
+  }
+
+  handleDelete(name, e) {
+    console.log(name, e)
+  }
+
   render() {
     return (
       <div className='red'>
@@ -44,7 +65,33 @@ class Webpack extends React.Component {
           onClick={this.handleClick}
           dangerouslySetInnerHTML={ this.getButtonText() }
         />
+        <button
+          onClick={(e) => this.handleArrowClick('webpack', e)}
+        >
+          webpack
+        </button>
+        <button
+          onClick={this.handleDelete.bind(this, 'delete')}
+        >
+          删除
+        </button>
       </div>
+    )
+  }
+}
+
+class Form extends React.Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('You clicked submit.')
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <button type='submit'>submit</button>
+      </form>
     )
   }
 }
@@ -54,6 +101,7 @@ const app = ReactDOMClient.createRoot(document.getElementById('app'))
 app.render(
   <React.StrictMode>
     <Webpack />
+    <Form />
     <svg>
       <use xlinkHref="#icon-bug" />
     </svg>
